127e4015366d911131ff2d9234c5ecfc2276ebbc
ZhangSongZ <583656773@qq.com>
2026-05-21 14:21:48 +0800

    我来了

--- public/assets/js/backend/csmradmin/adminapply.js
+++ public/assets/js/backend/csmradmin/adminapply.js
@@ -76,7 +76,7 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                                 },
                                 {
                                     name: 'submitauditreturn',
-                                    text: __('审核退回'),
+                                    text: __('审核退回1'),
                                     classname: 'btn btn-xs btn-danger btn-dialog',
                                     icon: 'fa fa-file',
                                     url: 'csmradmin/adminapply/submitauditreturn',
